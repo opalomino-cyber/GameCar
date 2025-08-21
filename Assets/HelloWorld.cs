bf91b9f8610f2b510a4e2bb73dd928e2b66910a7
opalomino7 <opalomino7@gmail.com>
2025-08-21 09:07:52 -0600

    Added project files

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // List of players
+        int playOne, playTwo, playThree, playFour;
+
+        playOne = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
